b8106aa2ced3bd04310c218d34a447539ad33279
Anup Barua <anupbarua30@gmail.com>
2024-12-14 18:18:00 +0600

    day7

--- week 7/day7/problem 21, C. Bitwise Balancing.cpp
+++ week 7/day7/problem 21, C. Bitwise Balancing.cpp
@@ -6,21 +6,31 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-		
-	int tc=1;
+
+	int tc = 1;
 	cin >> tc;
-	while (tc--){
-		ll a, b, c, d;
+	while (tc--) {
+		ll a = 0, b, c, d;
 		cin >> b >> c >> d;
-		a= 0ll;
-		for(ll i=0; i<64; i++){
-			ll aa = (1ll<<i);
-			ll bb = (aa & b)? aa: 0ll;
-			ll cc = (aa & c)? aa: 0ll;
-			ll dd = (aa & d)? aa: 0ll;
-			if((aa|bb)== dd+(aa &cc)) a+=aa;			
+		for (ll i = 0; i < 64; i++) {
+			ll a1 = (1LL << i);
+			ll b1 = (a1 & b) ? a1 : 0LL;
+			ll c1 = (a1 & c) ? a1 : 0LL;
+			ll d1 = (a1 & d) ? a1 : 0LL;
+			if ((a1 | b1) == d1 + (a1 & c1)) a += a1;
 		}
-		if((a|b)==(d+(a&c))) cout << a << endl;		
+		if ((a | b) == (d + (a & c))) cout << a << endl;
 		else cout << -1 << endl;
 	}
 }
+
+/*
+3
+2 2 2
+4 2 6
+10 2 14
+
+0
+-1
+12
+*/
